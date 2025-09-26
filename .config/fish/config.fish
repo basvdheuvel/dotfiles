a0f8b08190b001b473506b2970a5202932489614
Bas <vdheuvel.bas@gmail.com>
2025-09-26 13:21:11 +0200

    Prep for denkpet

--- .config/fish/config.fish
+++
@@ -1,6 +0,0 @@
-set -x VISUAL nvim
-set -x EDITOR $VISUAL
-
-alias tlmgr='/usr/share/texmf-dist/scripts/texlive/tlmgr.pl --usermode'
-
-alias urldecode='python -c "import sys, urllib.parse as ul; [sys.stdout.write(ul.unquote_plus(l)) for l in sys.stdin]"'
