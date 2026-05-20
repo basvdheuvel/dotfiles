c369ca230145bcf3a55a475785d1fedc1b271dcf
Bas <vdheuvel.bas@gmail.com>
2026-05-20 10:21:00 +0200

    Axini updates
    Nvim: softwrap navigation in visual mode
    
    Nvim: small things
    
    Track Fish configs
    
    Nvim spell
    
    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,13 @@ set -x EDITOR $VISUAL
 alias tlmgr='/usr/share/texmf-dist/scripts/texlive/tlmgr.pl --usermode'
 
 alias urldecode='python -c "import sys, urllib.parse as ul; [sys.stdout.write(ul.unquote_plus(l)) for l in sys.stdin]"'
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+source /home/bas/.config/fish/functions/bundle.fish
+fish_add_path /home/bas/.local/bin
+set -U FZF_LEGACY_KEYBINDINGS 0
+rvm default
+nvm use default >/dev/null
